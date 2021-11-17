65a97d4880cd6c71fe7fb55147e7621120c884ca
gccornejo441 <gcornejo441@gmail.com>
2021-11-16 19:41:24 -0800

    setting up for web design layout. This includes styled comps and twcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+// gatsby-config.js
 module.exports = {
   plugins: [
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+        tailwind: true,
+        printRejected: true,
+        purgeCSSOptions: {
+          safelist: [],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
       },
     },
+    "gatsby-plugin-postcss",
   ],
 }
