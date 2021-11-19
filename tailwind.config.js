351fb216c85ced19bfcca0c2f0fd1473cfb51076
gccornejo441 <gcornejo441@gmail.com>
2021-11-18 18:08:56 -0800

    restructing to use next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
-module.exports = () => ({
-  plugins: [require("tailwindcss")],
-})
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
