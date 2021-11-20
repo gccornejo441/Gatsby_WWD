de547e08b2de40ad78d4e86434e87098386974d8
gccornejo441 <gcornejo441@gmail.com>
2021-11-19 22:03:17 -0800

    adding navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    gradientColorStops: (theme) => ({
+      ...theme("colors"),
+      greeny: "#5CBDB9",
+      lightGreeny: "#9DCCCA",
+      rosey: "#e3342f",
+    }),
   },
   variants: {
     extend: {},
