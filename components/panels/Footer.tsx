e8ee39b64c72e36c5253e3a0968ad2b9f4172beb
gccornejo441 <gcornejo441@gmail.com>
2021-11-28 20:26:00 -0800

    Homepage footer

--- components/panels/Footer.tsx
+++ components/panels/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import WebWorksDream from "../../public/images/WWD.2.svg";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 
 export const Footer = () => {
   return (
@@ -11,35 +11,43 @@ export const Footer = () => {
           <div className="flex px-2 relative pb-12 w-1/2 md:w-1/4">
             <div>
               <div className="mb-7">
-                <a className="cursor-pointer">
-                  <Link href="/">
+                <Link href="/">
+                  <a className="cursor-pointer">
                     <Image
                       src={WebWorksDream}
                       alt="WebWorks Dreams Logo"
                       height="24"
                       width="28"
                     />
-                  </Link>
-                </a>
+                  </a>
+                </Link>
               </div>
               <ul className="m-0 p-0 list-none block">
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">
-                    <Link href="/">Download</Link>
-                  </a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Home</a>
+                  </Link>
                 </li>
 
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Web app</a>
+                  <Link href="/">
+                    <a className="cursor-pointer">About</a>
+                  </Link>
                 </li>
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Pricing</a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Portfolio</a>
+                  </Link>
                 </li>
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Server status</a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Web Design</a>
+                  </Link>
                 </li>
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Changelog</a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Our Story</a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -47,116 +55,105 @@ export const Footer = () => {
           <div className="flex px-2 relative pb-12 w-1/2 md:w-1/4">
             <div>
               <h2 className="md:text-2xl text-xl mb-8 font-sans font-semibold leading-tight text-gray-600">
-                Product
+                Inventory
               </h2>
               <ul className="m-0 p-0 list-none block">
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-500 pb-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">
-                    <Link href="/">Download</Link>
-                  </a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Pricing</a>
+                  </Link>
                 </li>
 
                 <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Web app</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Pricing</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Server status</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Changelog</a>
+                  <Link href="/">
+                    <a className="cursor-pointer">Products</a>
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="flex px-2 relative pb-12 w-1/2 md:w-1/4">
-            <div>
-              <h2 className="md:text-2xl text-xl mb-8 font-sans font-semibold leading-tight text-gray-600">
-                Product
-              </h2>
-              <ul className="m-0 p-0 list-none block">
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-500 pb-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">
-                    <Link href="/">Download</Link>
-                  </a>
-                </li>
-
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Web app</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Pricing</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Server status</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Changelog</a>
-                </li>
-              </ul>
-            </div>
           </div>
-          <div className="flex px-2 relative pb-12 w-1/2 md:w-1/4">
-            <div>
-              <h2 className="md:text-2xl text-xl mb-8 font-sans font-semibold leading-tight text-gray-600">
-                Product
-              </h2>
-              <ul className="m-0 p-0 list-none block">
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-500 pb-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">
-                    <Link href="/">Download</Link>
-                  </a>
-                </li>
-
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Web app</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Pricing</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Server status</a>
-                </li>
-                <li className="m-0 tracking-normal text-gray-500 hover:text-gray-600 py-2 font-normal leading-5 text-base md:mb-1">
-                  <a className="cursor-pointer">Changelog</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <ul>
-            <li>
-              <a href="/terms-of-service" className="sc-6w6m2h-0 ggzfjo">
-                Terms of service
-              </a>
-            </li>
-            <li>
-              <a href="/privacy-policy" className="sc-6w6m2h-0 ggzfjo">
-                Privacy policy
-              </a>
-            </li>
-            <li>
-              <a href="/cookie-policy" className="sc-6w6m2h-0 ggzfjo">
-                Cookies
-              </a>
-            </li>
-            <li>
+          <div className="text-xs pt-6 pb-20 flex justify-between">
+            <div className="flex">
               <a
-                href="/license-acknowledgements"
+                target="_blank"
+                href="https://twitter.com/avocode"
+                aria-label="twitter"
                 className="sc-6w6m2h-0 ggzfjo"
               >
-                Licenses
+                Twitter
               </a>
-            </li>
-            <li>
-              <a href="/sitemap" className="sc-6w6m2h-0 ggzfjo">
-                Sitemap
+              <a
+                target="_blank"
+                href="https://www.facebook.com/avocode/"
+                aria-label="facebook"
+                className="sc-6w6m2h-0 ggzfjo"
+              >
+                Facebook
               </a>
-            </li>
-          </ul>
-          <p className="sc-1fvlrgk-0 hhCuPm">Copyright © Avocode</p>
-        </div>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/avocode/"
+                aria-label="instagram"
+                className="sc-6w6m2h-0 ggzfjo"
+              >
+                Instagram
+              </a>
+              <a
+                target="_blank"
+                href="https://www.youtube.com/channel/UCaNsjhUdh3BxFL6tzyuhc9g"
+                aria-label="youtube"
+                className="sc-6w6m2h-0 ggzfjo"
+              >
+                YouTube
+              </a>
+              <a
+                target="_blank"
+                href="https://dribbble.com/avocode"
+                aria-label="dribbble"
+                className="sc-6w6m2h-0 ggzfjo"
+              >
+                Dribble
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/company/avocode-inc-"
+                aria-label="linkedin"
+                className="sc-6w6m2h-0 ggzfjo"
+              >
+                Linkedin
+              </a>
+            </div>
+            <ul className="flex p-0 m-0 text-gray-500 ">
+              <li className="block sm:inline-block pl-2">
+                <a href="/terms-of-service" className="sc-6w6m2h-0 ggzfjo">
+                  Terms of service
+                </a>
+              </li>
+              <li className="block sm:inline-block pl-2">
+                <a href="/privacy-policy" className="sc-6w6m2h-0 ggzfjo">
+                  Privacy policy
+                </a>
+              </li>
+              <li className="block sm:inline-block pl-2">
+                <a href="/cookie-policy" className="sc-6w6m2h-0 ggzfjo">
+                  Cookies
+                </a>
+              </li>
+              <li className="block sm:inline-block pl-2">
+                <a href="/license-acknowledgements" className="sc-6w6m2h-0 ggzfjo">
+                  Licenses
+                </a>
+              </li>
+              <li className="block sm:inline-block pl-2">
+                <a href="/sitemap" className="sc-6w6m2h-0 ggzfjo">
+                  Sitemap
+                </a>
+              </li>
+            </ul>
+            <p className="sm:inline-block text-sm font-semibold text-gray-500 m-0">Copyright © 2021 Avocode</p>
+          </div>
+        
       </div>
     </footer>
   );
