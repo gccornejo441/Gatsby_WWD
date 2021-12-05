8ebb7b4108e1ad65ff999657bd245baaeb664343
gccornejo441 <gcornejo441@gmail.com>
2021-12-04 19:10:45 -0800

    adding preloader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,45 @@
+import * as React from 'react';
 import Head from "next/head";
 import { Hero } from "../components/panels/Hero";
 import { About } from "../components/panels/AboutUs";
 import { Shapes } from "../components/shapes/Shapes";
+import { PreLoader } from "../components/shapes/PreLoader";
 
-export default function Home() {
+interface PageProps {
+  load: boolean,
+}
+
+const Home: React.FC<PageProps> = () => {
+  const [ onLoad, setLoad ] = React.useState<PageProps>({ load: false });
+  
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoad({ load: true })
+    }, 1500);
+  }, []);
+
+  console.log(onLoad);
+
   return (
     <>
-      <Head>
-        <title>WebWorks Dreams</title>
-      </Head>
-      <main className="flex flex-col">
-        <Shapes />
-        <Hero />
-        <About />
-      </main>
+      {onLoad.load ? (
+        <>
+          <PreLoader />
+        </>
+      ) : (
+        <>
+          <Head>
+            <title>WebWorks Dreams</title>
+          </Head>
+          <main className="flex flex-col">
+            <Shapes />
+            <Hero />
+            <About />
+          </main>
+        </>
+      )}
     </>
   );
 }
+
+export default Home;
