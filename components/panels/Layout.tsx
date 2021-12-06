410177814902cce3ffa73efc2d67f80f469cf1dc
gccornejo441 <gcornejo441@gmail.com>
2021-12-05 19:30:20 -0800

    update to preloader

--- components/panels/Layout.tsx
+++ components/panels/Layout.tsx
@@ -7,42 +7,14 @@ type Props = {
   children: React.ReactNode;
 };
 
-interface PageProps {
-  load: boolean;
-}
-
-export const Layout: React.FC<Props & PageProps> = ({ children }: Props) => {
-  const [onLoad, setLoad] = React.useState<PageProps>({ load: true });
-  React.useEffect(() => {
-    window.addEventListener("load", (evt) => {
-      if (evt.type == "load") {
-        setLoad({
-          ...onLoad,
-          load: false,
-        });
-      }
-    });
-  }, []);
-
+export const Layout: React.FC<Props> = ({ children }: Props) => {
   return (
     <>
-      {onLoad.load ? (
-        <div className="h-screen flex justify-center items-center smooch-text">
-          <PreLoader />
-          <span className="text-greeny font-normal text-5xl md:text-7xl pl-28">
-            Loading
-            <span className="load_dot">.</span>
-          </span>
-        </div>
-      ) : (
-        <>
-          <div>
-            <NavBar />
-            <main className="pt-16 md:pt-0">{children}</main>
-            <Footer />
-          </div>
-        </>
-      )}
+      <div>
+        <NavBar />
+        <main className="pt-16 md:pt-0">{children}</main>
+        <Footer />
+      </div>
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,36 @@
+import * as React from "react";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import { Layout } from "../components/panels/Layout";
+import { PreLoader } from "../components/shapes/PreLoader";
+
+interface PageProps {
+  load: boolean;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {loading ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
+        <div className="h-screen flex flex-col md:flex-row justify-center items-center smooch-text">
+          <PreLoader />
+          <span className="text-greeny font-normal text-5xl md:text-7xl pt-60 md:pl-28">
+            Loading...
+          </span>
+        </div>
+      )}
+    </>
   );
 }
 
