f77ca60c9d0b237e407bef1cfb0e9b4ce74b60f6
gccornejo441 <gcornejo441@gmail.com>
2021-12-22 20:47:58 -0800

    adding fb_id meta tag

--- components/panels/Layout.tsx
+++ components/panels/Layout.tsx
@@ -26,22 +26,23 @@ export const Layout: React.FC<Props> = ({
           <meta name="robots" content="all" />
 
           {/* OG */}
+          <meta property="fb:app_id" content="254907099748012" />
+          <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.webworksdreams.com/" />
-          <meta
-            property="og:image"
-            content="https://www.webworksdreams.com/images/sharecard.jpg"
-          />
-          <meta property="og:site_name" content="WebWorks Dreams" />
           <meta
             property="og:title"
             content="Welcome to WebWorks Dreams where dreams come true | WebWorks Dreams"
           />
-          <meta property="og:type" content="website" />
-
+          <meta
+            property="og:image"
+            content="https://www.webworksdreams.com/images/sharecard.jpg"
+          />
           <meta
             name="description"
             content="WebWorks Dreams is an outstanding web development and design company providing superior web services to non-profits &amp; startups for success in the digital age."
           />
+          <meta property="og:site_name" content="WebWorks Dreams" />
+
           {/* Twitter */}
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@WebWorksDreams" />
