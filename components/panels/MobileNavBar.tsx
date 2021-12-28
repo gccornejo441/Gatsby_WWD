45a6f401ebfc62dcf44e76011b583a3c04f3444b
gccornejo441 <gcornejo441@gmail.com>
2021-12-27 22:00:48 -0800

    using framer-motion and some functionality to add some ui

--- components/panels/MobileNavBar.tsx
+++ components/panels/MobileNavBar.tsx
@@ -1,100 +1,143 @@
 import { Menu, Transition } from "@headlessui/react";
-import { FiMenu } from "react-icons/fi";
-import { Fragment, useState } from "react";
+import { Fragment, useRef } from "react";
 import Image from "next/image";
 import WebWorksDreamsLogo from "../../public/images/WWD.2.svg";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const Path = (props: any) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="hsl(0, 0%, 18.03921568627451%)"
+    strokeLinecap="round"
+    {...props}
+  />
+);
 
 export const MobileNavBar = () => {
+  const containerRef = useRef(null);
   return (
     <Menu
       as="div"
       className="justify-center text-white text-xs bg-gradient-to-r from-greeny to-lightGreeny fixed z-10 w-full"
     >
-      <>
-        <div className="flex justify-between px-4">
-          <Link href="/" passHref>
-            <div className="flex items-center cursor-pointer">
-              <Image
-                src={WebWorksDreamsLogo}
-                width="30"
-                height="30"
-                alt="WebWorks Dreams Logo"
-              />
-              <span className="text-lg text-white ml-1">WebWorks Dreams</span>
-            </div>
-          </Link>
-          <Menu.Button>
-            <FiMenu aria-label="Menu Button" className="h-12 w-auto py-1" />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items static>
-            <div className="text-2xl bg-white text-darkestGreeny font-semibold h-auto text-md flex flex-col absolute z-10 w-full drop-shadow-2xl">
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/">
-                    <a
-                      className={`${
-                        active && "bg-greeny text-white"
-                      } ${"py-3 px-4"}`}
-                    >
-                      Home
-                    </a>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/services/web-services">
-                    <a
-                      className={`${
-                        active && "bg-greeny text-white"
-                      } ${"py-3 px-4"}`}
-                    >
-                      Services
-                    </a>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/contact">
-                    <a
-                      className={`${
-                        active && "bg-greeny text-white"
-                      } ${"py-3 px-4"}`}
-                    >
-                      Contact Us
-                    </a>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <Link href="/contact">
-                    <a
-                      className={`${
-                        active && "bg-greeny text-white"
-                      } ${"py-3 px-4"}`}
-                    >
-                      Get A Quote
-                    </a>
-                  </Link>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </>
+      {({ open }) => (
+        <>
+          <div className="flex justify-between px-4">
+            <Link href="/" passHref>
+              <div className="flex items-center cursor-pointer">
+                <Image
+                  src={WebWorksDreamsLogo}
+                  width="30"
+                  height="30"
+                  alt="WebWorks Dreams Logo"
+                />
+                <span className="text-lg text-gray-800 ml-1">
+                  WebWorks Dreams
+                </span>
+              </div>
+            </Link>
+            <Menu.Button>
+              <motion.div animate={open ? "open" : "closed"} ref={containerRef}>
+                <button
+                  className="p-3 my-2 rounded-full bg-teal-100"
+                  aria-label="Menu Button"
+                >
+                  <svg width="23" height="23" viewBox="0 0 19 19">
+                    <Path
+                      variants={{
+                        closed: { d: "M 2 2.5 L 20 2.5" },
+                        open: { d: "M 3 16.5 L 17 2.5" },
+                      }}
+                    />
+                    <Path
+                      d="M 2 9.423 L 20 9.423"
+                      variants={{
+                        closed: { opacity: 1 },
+                        open: { opacity: 0 },
+                      }}
+                      transition={{ duration: 0.1 }}
+                    />
+                    <Path
+                      variants={{
+                        closed: { d: "M 2 16.346 L 20 16.346" },
+                        open: { d: "M 3 2.5 L 17 16.346" },
+                      }}
+                    />
+                  </svg>
+                </button>
+              </motion.div>
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items static>
+              <div className="text-lg bg-white text-darkestGreeny font-semibold h-auto text-md flex flex-col absolute z-10 w-full drop-shadow-2xl">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/">
+                      <a
+                        className={`${
+                          active && "bg-greeny text-white"
+                        } ${"py-3 px-4 border-b-2 border-gray-100"}`}
+                      >
+                        Home
+                      </a>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/services/web-services">
+                      <a
+                        className={`${
+                          active && "bg-greeny text-white"
+                        } ${"py-3 px-4 border-b-2 border-gray-100"}`}
+                      >
+                        Services
+                      </a>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/contact">
+                      <a
+                        className={`${
+                          active && "bg-greeny text-white"
+                        } ${"py-3 px-4 border-b-2 border-gray-100"}`}
+                      >
+                        Contact Us
+                      </a>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/contact">
+                      <a
+                        className={`${
+                          active && "bg-greeny text-white"
+                        } ${"py-3 px-4"}`}
+                      >
+                        Get A Quote
+                      </a>
+                    </Link>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
     </Menu>
   );
 };
