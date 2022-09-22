2d49812c90d2e005643945a61336441fe01933b7
Gabriel Cornejo <gcornejo441@gmail.com>
2022-09-22 08:32:15 -0700

    g tag update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -76,7 +76,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           />
           <Script
             strategy="afterInteractive"
-            src="https://www.googletagmanager.com/gtag/js?id=G-NXP8R0SFMW"
+            src="https://www.googletagmanager.com/gtag/js?id=G-PJGWP9G1LK"
           />
           <Script
           id="google-analytics"
@@ -87,7 +87,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
             
-              gtag('config', 'G-NXP8R0SFMW');
+              gtag('config', 'G-PJGWP9G1LK');
           `,
             }}
           />
