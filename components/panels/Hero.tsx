5789134a9eeff4179880fca8df76bfa64671f878
Gabriel Cornejo <gcornejo441@gmail.com>
2023-08-13 07:25:26 -0700

    updating copy on landing page

--- components/panels/Hero.tsx
+++ components/panels/Hero.tsx
@@ -91,9 +91,7 @@ export const Hero = () => {
             </div>
             <div className="text-center flex justify-center">
               <div className="text-slate-900 font-bold text-xl lg:text-2xl lg:max-w-4xl px-4">
-                <p>&quot;If you&apos;re in the market for modern solutions, WebWorks
-                  Dreams offers the means to empower nonprofits to explore the
-                  newest and innovative software capabilities.&quot;</p>
+                <p>&quot;Are you in search of cutting-edge solutions? Look no further than WebWorks Dreams. We specialize in equipping non-profit entities with the tools to unlock the potential of the latest and most innovative software capabilities.&quot;</p>
               </div>
             </div>
             <div>
